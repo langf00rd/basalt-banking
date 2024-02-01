1c5ab0cd72be78c6aef46c7c150baf83fa91cf17
langford <langfordquarshie21@gmail.com>
2024-02-01 18:00:02 +0000

    chore: show chatbox fab on all pages

--- components/layouts/main-layout.tsx
+++ components/layouts/main-layout.tsx
@@ -3,30 +3,35 @@ import Aside from "../aside";
 import Header from "../header";
 import { cn } from "@/lib/utils/cn";
 import Head from "next/head";
+import ChatBox from "../chat-box";
 
 export default function MainLayout(props: {
    className?: string;
    children: ReactNode;
-   title?: string;
+   title: string;
 }) {
+   const metaTitle = props.title ? `Basalt | ${props.title}` : "Basalt";
    return (
-      <main className="flex h-screen">
+      <>
          <Head>
-            <title>Basalt | {props.title}</title>
+            <title>{metaTitle}</title>
          </Head>
-         <Aside />
-         <div className="flex-[5] p-10 overflow-y-scroll">
-            <div className="space-y-5">
-               <Header />
-               <div className="pt-10 space-y-5">
-                  <h1 className="text-3xl text-black">{props.title}</h1>
-                  <div className={cn("space-y-10", props.className)}>
-                     {props.children}
+         <main className="flex h-screen">
+            <ChatBox />
+            <Aside />
+            <div className="flex-[5] p-10 overflow-y-scroll">
+               <div className="space-y-5">
+                  <Header />
+                  <div className="pt-10 space-y-5">
+                     <h1 className="text-3xl text-black">{props.title}</h1>
+                     <div className={cn("space-y-10", props.className)}>
+                        {props.children}
+                     </div>
                   </div>
                </div>
             </div>
-         </div>
-         {/* <CurrencyNews /> */}
-      </main>
+            {/* <CurrencyNews /> */}
+         </main>
+      </>
    );
 }
